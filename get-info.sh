21b9b3cdc54638ce3721ca8b4e08c7360a7fa72b
vedranlebo <vedran.lebo@gmail.com>
2017-03-16 12:55:03 +0100

    Added get-info script

---
+++ get-info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for inst in `aws ec2 describe-instances  | grep 'InstanceId' | awk '{print $2 }' | sed 's/[\",]//g' `; do
+	aws ec2 describe-instances --filter Name="instance-id",Values="${inst}" > ${inst}.json
+
+	echo '||--------------------------------EC2 info---------------------------------------------||'
+	echo "Name: $(cat ${inst}.json | jq -c '.Reservations[0].Instances[0].Tags[] | select(.Key =="Name").Value')"
+	echo "ID: $(cat ${inst}.json | jq -c '.Reservations[0].Instances[0].InstanceId')"
+	echo "Instancte Size: $(cat ${inst}.json | jq -c '.Reservations[0].Instances[0].InstanceType ')"
+	echo "VPC: $(cat ${inst}.json | jq -c '.Reservations[0].Instances[0].VpcId ')"
+	rm ${inst}.json
+done
